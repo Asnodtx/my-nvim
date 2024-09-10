4bbcea183ecd2cb5a3aaec3bcbf3480ce3986658
Asnodtx <marentes13@outlook.com>
2024-09-10 17:37:51 -0600

    first commit

---
+++ lua/config/options.lua
@@ -0,0 +1,27 @@
+vim.opt.mouse = ""
+vim.opt.number = true
+vim.opt.relativenumber = true
+vim.opt.nu = true
+
+vim.opt.tabstop = 2
+vim.opt.softtabstop = 2
+vim.opt.shiftwidth = 2
+vim.opt.expandtab = true
+
+vim.opt.smartindent = true
+
+vim.opt.wrap = false
+
+vim.opt.swapfile = false
+vim.opt.backup = false
+
+vim.opt.termguicolors = true
+
+
+vim.opt.scrolloff = 8
+vim.opt.signcolumn = "yes"
+vim.opt.clipboard = "unnamedplus"
+vim.g.mapleader = " "
+vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
+vim.keymap.set('n', '<C-h>', '<C-w>h', {})
+vim.keymap.set('n', '<C-l>', '<C-w>l',{})
---
+++ lua/plugins/lspzero.lua
@@ -0,0 +1,44 @@
+return {
+	{
+		"williamboman/mason.nvim",
+		lazy = false,
+		opts = {
+			ensure_installed = {
+				"eslint-lsp",
+				"prettierd",
+				"tailwindcss-language-server",
+				"typescript-language-server",
+			},
+		},
+		config = function()
+			require("mason").setup()
+		end,
+	},
+	{
+		"williamboman/mason-lspconfig.nvim",
+		lazy = false,
+		opts = {
+			auto_install = true,
+		},
+	},
+	{
+		"neovim/nvim-lspconfig",
+		lazy = false,
+		config = function()
+			local lspconfig = require("lspconfig")
+
+			local servers = { "ts_ls", "tailwindcss", "eslint" }
+
+			for _, lsp in ipairs(servers) do
+				lspconfig[lsp].setup({
+					on_attach = on_attach,
+					capabilities = capabilities,
+				})
+			end
+			vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
+			vim.keymap.set("n", "<leader>gd", vim.lsp.buf.definition, {})
+			vim.keymap.set("n", "<leader>gr", vim.lsp.buf.references, {})
+			vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, {})
+		end,
+	},
+}
